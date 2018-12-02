fa88f87014ab270f0d7c9ee707003d3645bc02e4
saton <1131990601@qq.com>
2018-12-02 23:09:26 +0800

    更新代码

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,11 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author 张廷强
+ * @date 2018/12/2 17:05
+ */
+public interface BrandMapper extends Mapper<Brand> {
+}
