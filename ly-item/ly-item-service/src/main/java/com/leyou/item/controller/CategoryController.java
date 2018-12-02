d55e93b983d2a2d860baaa76225e00dc09c89b7a
saton <1131990601@qq.com>
2018-12-02 18:31:43 +0800

    更新代码

--- ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -1,14 +1,38 @@
 package com.leyou.item.controller;
 
+import com.leyou.item.pojo.Category;
 import com.leyou.item.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author 张廷强
  * @date 2018/12/2 17:08
  */
+@RestController
+@RequestMapping("category")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("list")
+    public ResponseEntity<List<Category>> queryByParentId(
+            @RequestParam(value = "pid", defaultValue = "0") Long pid) {
+        List<Category> list = this.categoryService.queryListByParent(pid);
+        if (list == null || list.size() < 1) {
+            //没找到  返回状态码notfound(404)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        //找到返回200++数据
+        return ResponseEntity.ok(list);
+    }
+
 }
     
--- ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -1,14 +1,25 @@
 package com.leyou.item.service;
 
 import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author 张廷强
  * @date 2018/12/2 17:07
  */
+@Service
 public class CategoryService {
     @Autowired
    private CategoryMapper categoryMapper;
+
+    public List<Category> queryListByParent(Long pid) {
+        Category category = new Category();
+        category.setParentId(pid);
+        return this.categoryMapper.select(category);
+    }
 }
     
