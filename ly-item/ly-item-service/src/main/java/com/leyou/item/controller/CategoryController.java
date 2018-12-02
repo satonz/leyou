0894fb82f9100f83e7b9b7aa41879ed20a7c435f
saton <1131990601@qq.com>
2018-12-02 17:11:10 +0800

    更新代码

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -0,0 +1,14 @@
+package com.leyou.item.controller;
+
+import com.leyou.item.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author 张廷强
+ * @date 2018/12/2 17:08
+ */
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+}
+    
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author 张廷强
+ * @date 2018/12/2 17:07
+ */
+public class CategoryService {
+    @Autowired
+   private CategoryMapper categoryMapper;
+}
+    
